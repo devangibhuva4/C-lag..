1b059e11db5c1158b8f8d410e9653255da6f4d5d
devangibhuva4 <134709804+devangibhuva4@users.noreply.github.com>
2023-06-07 10:13:17 -0400

    Add files via upload

---
+++ day10.c/main (20).c
@@ -0,0 +1,90 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+   char a;
+   
+   switch(a){
+       
+       
+       case 'j':
+       printf("january");
+       break;
+       
+       case 'f':
+       printf("february");
+       break;
+       
+       case'p':
+       printf("march");
+       break;
+       
+       case 'a':
+       printf("april");
+       break;
+       
+       case 'm':
+       printf("may");
+       break;
+       
+       case 'z':
+       printf("jun");
+       break;
+       
+       case 'b':
+       printf("july");
+       break;
+       
+       case 'l':
+       printf("august");
+       break;
+       
+       case 's':
+       printf("september");
+       break;
+       
+       case 'o':
+       printf("october");
+       break;
+       
+       case 'n':
+       printf("november");
+       break;
+       
+       case 'd':
+       printf("december");
+       break;
+    
+      
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+   }
+   
+   
+
+    return 0;
+}
---
+++ day10.c/main (21).c
@@ -0,0 +1,153 @@
+
+
+#include <stdio.h>
+
+int main(){
+    
+     int c;
+     
+     printf("welcome to  train service\n");
+     
+     
+     printf("1.tejas\n");
+     printf("2. super fast\n");
+     
+     scanf("%d",&c);
+     
+     switch(c){
+         
+         case 1:
+         
+             
+            printf(" enter 3 for sleeping class\n");
+             printf(" enter 4 sitting  class\n");
+             scanf("%d",&c);
+              switch(c){
+                case 3:
+                
+                 printf("enter 5 for ahmdabad\n");
+                 printf("enter 6 for mubai\n");
+                 printf("enter 7 for vadodara\n");
+                 printf("enter 8 for surat\n");
+                 
+                  scanf("%d",&c);
+                  switch(c){
+                      
+                     case 5:
+                     printf("899");
+                     break;
+                     case 6:
+                     printf("699");
+                     break;
+                     case 7:
+                     printf("1000");
+                     break;
+                     case 8:
+                     printf("999");
+                     break;
+                     
+                     break; 
+                  }
+                  
+                 case 4:
+                  
+                  printf("enter 9 forahmdabad\n");
+                 printf(" enter 10 mubai\n");
+                 printf(" enter 11 vadodara\n");
+                 printf(" enter 12 surat\n");
+                 
+                  scanf("%d",&c);
+                  switch(c){
+                      
+                     case 9:
+                     printf("899");
+                     break;
+                     case 10:
+                     printf("699");
+                     break;
+                     case 11:
+                     printf("1000");
+                     break;
+                     case 12:
+                     printf("999");
+                     break;
+                     
+                     break; 
+                  }
+                  
+              }
+              break;
+               
+               case 2:
+               
+             printf("enter 13sleeping class\n");
+             printf("enter 14 sitting  class\n");
+             scanf("%d",&c);
+             
+             
+              switch(c){
+                  
+                 case 13:
+                
+                 printf("enter 15 ahmdabad\n");
+                 printf("enter 16 mumbai\n");
+                 printf("enter 17 vadodara\n");
+                 printf("enter 18 surat\n");
+                 
+                  scanf("%d",&c);
+                  switch(c){
+                      
+                     case 15:
+                     printf("899");
+                     break;
+                     case 16:
+                     printf("699");
+                     break;
+                     case 17:
+                     printf("1000");
+                     break;
+                     case  18:
+                     printf("999");
+                     break;
+                     
+                     break; 
+                  }
+                  
+              
+                  
+              }
+               
+               break;
+                  
+                 case 14:
+                  printf("enter 19 gj5\n");
+                 printf("enter 20 up\n");
+                 printf("enter 21 bihar\n");
+                 printf("enter 22 rj\n");
+                 
+                  scanf("%d",&c);
+                  switch(c){
+                      
+                     case 19:
+                     printf("399");
+                     break;
+                     case 20:
+                     printf("299");
+                     break;
+                     case 21:
+                     printf("8000");
+                     break;
+                     case 22:
+                     printf("599");
+                     break;
+                     
+                     break; 
+                  }
+         
+         
+         
+     }
+    
+
+    return 0;
+}
